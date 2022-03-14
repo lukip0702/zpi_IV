7a0b3d71da1586bb22fe2cd0536bcca81f071ed7
lukip0702 <luki33221@student.kul.lublin.pl>
2022-03-14 18:34:58 +0100

    Two new lines

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Linia gałęzi z secondBranch");
+        System.out.println("2 Linia gałęzi z secondBranch");
     }
 }
